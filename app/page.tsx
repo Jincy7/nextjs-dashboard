051cbd28a89605c754567ab3cac947fd25fb1b4a
Jin Chang Yeob <jincy713@gmail.com>
2023-11-27 14:56:07 +0900

    chap 3: Image 적용하기

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import {lusitana} from "@/app/ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -29,6 +30,8 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            <Image src="/hero-desktop.png" alt="Screenshot of the dashboard project" className="hidden md:block" width={1000} height={760}></Image>
+            <Image src="/hero-mobile.png" alt="Screenshot of the dashboard project" className="block md:hidden" width={560} height={620}></Image>
           {/* Add Hero Images Here */}
         </div>
       </div>
